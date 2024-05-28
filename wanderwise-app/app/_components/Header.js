c6f942dcca37b5ccdfb5225b3a7edd5ed4f2f86c
psantos2107 <paul.santos2107@gmail.com>
2024-05-27 22:15:31 -0700

    more header adjustments

--- wanderwise-app/app/_components/Header.js
+++ wanderwise-app/app/_components/Header.js
@@ -3,10 +3,12 @@ import logo from "@/public/imgs/airplane_logo.jpg";
 
 const Header = () => {
   return (
-    <header className="px-4 py-2 lg:py-5 bg-slate-50 flex justify-between h-1/6 w-full border-solid border-gray-600 border-2 shadow">
-      <div className="flex gap-4 justify-center">
-        <h1 className="self-center text-3xl lg:text-6xl">WANDERWISE</h1>
-        <div className="relative lg:w-1/12 w-1/6">
+    <header className="px-4 py-2 lg:py-5 bg-slate-50 flex justify-center h-1/6 w-full border-solid border-gray-600 border-2 shadow max-w-screen-xl mx-auto">
+      <div className="flex gap-4 lg:gap-12 justify-center">
+        <h1 className="self-center text-3xl lg:text-6xl lg:tracking-widest">
+          WANDERWISE
+        </h1>
+        <div className="relative w-16 md:w-24 lg:w-20">
           <Image
             src={logo}
             alt="Airplane Logo"
